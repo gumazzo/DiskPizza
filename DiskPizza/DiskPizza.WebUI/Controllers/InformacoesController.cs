2cc3fc666634f4d59631fab1b1759b2458754f22
gumazzo <gustavo_roberto15@hotmail.com>
2018-04-05 21:35:06 -0300

    Disk Pizza

---
+++ DiskPizza/DiskPizza.WebUI/Controllers/InformacoesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiskPizza.WebUI.Controllers
+{
+    public class InformacoesController : Controller
+    {
+        // GET: Informacoes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
