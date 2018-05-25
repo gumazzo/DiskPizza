1574775b5d61e55f9f00950604cdd5dc03dd80e0
gumazzo <gustavo_roberto15@hotmail.com>
2018-05-24 21:09:46 -0300

    Ah sla não fiz muito... desanimei rs

--- DiskPizza/DiskPizza.WebUI/Controllers/InformacoesController.cs
+++ DiskPizza/DiskPizza.WebUI/Controllers/InformacoesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DiskPizza.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,8 @@ namespace DiskPizza.WebUI.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var lista = new Prod_x_TamanhoDAO().BuscarTodos();
+            return View( lista);
         }
     }
 }
